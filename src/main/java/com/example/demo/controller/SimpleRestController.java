dddcccd1feb8a73de2006eaf31987eaed4d2adb0
goentvy <entvyv@gmail.com>
2025-10-13 17:28:42 +0900

    251013 one more push

--- src/main/java/com/example/demo/controller/SimpleRestController.java
+++ src/main/java/com/example/demo/controller/SimpleRestController.java
@@ -19,9 +19,8 @@ public class SimpleRestController {
         return "Bye";
     }
 
-    // http://localhost:8080/article?title=스트링부트&content=완성하기
-    @RequestMapping(value="/article", method= RequestMethod.GET)
-    public String createArticle(@RequestParam("title") String title, @RequestParam("content") String content) {
+    @RequestMapping("/article")
+    public String createAritcle(@RequestParam("title") String title, @RequestParam("content") String content) {
         return String.format("title = %s / content = %s", title, content);
     }
 }
